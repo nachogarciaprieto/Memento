30f1783c433c2f5452f971fcdf3cc536251524c1
Alejandro Tejada <thertejada@gmail.com>
2019-05-03 13:39:21 +0200

    Revert "Commit para incluir la documentación Javadoc"
    This reverts commit 54cb8c2e82dc8f0d5d1f4aef7e69a5464b8f83aa.

--- src/Memento.java
+++ src/Memento.java
@@ -1,15 +1,3 @@
-/**
- * 
- * @author José Ignacio García Prieto
- * @author Alejandro Tejada Rodriguez
- * @author Alberto Sabuco Larrosa
- * @since 2019-04-30
- * @version 1.1 
- * Esta clase contiene dos métodos y el string.
- * Esta clase guarda el texto tecleado.
- */
-
-
 class Memento {
 
 	private String texto;
--- src/NotaDeTexto.java
+++ src/NotaDeTexto.java
@@ -1,48 +1,23 @@
-/**
- * 
- * @author José Ignacio García Prieto, , .
- * @author Alejandro Tejada Rodriguez
- * @author Alberto Sabuco Larrosa
- * @since 2019-04-30
- * @version 1.1 
- * Versión que incluye comentarios en JavaDoc y la documentación generada el JavaDoc.
- * Esta clase se encarga de recuperar los datos guardados en la clase Memento para mostrarla por pantalla.
- * Contiene 2 métodos Set y 2 métodos Get.
- */
 class NotaDeTexto {
 
 	private String texto;
 
-	/**
-	 * Método Set
-	 * @param texto
-	 */
+	// SET
 	public void setEstado(String texto) {
 		System.out.println("----\nNOTA DE TEXTO: Situando texto a:\n" + texto);
 		this.texto = texto;
 	}
 
-	/**
-	 * Método Get
-	 * @return
-	 */
+	// GET
 	private String getEstado() {
 		return texto;
 	}
-	
-	/**
-	 * Método Get
-	 * @return
-	 */
+
 	public Memento salvandoParaMemento() {
 		System.out.println("----\nNOTA DE TEXTO: Guardando a Memento.");
 		return new Memento(getEstado());
 	}
-	
-	/**
-	 * Método Set
-	 * @param m
-	 */
+
 	public void recuperandoDesdeMemento(Memento m) {
 		setEstado(m.getTextoSalvado());
 		System.out.println("----\nNOTA DE TEXTO: Texto despues de restaurar desde Memento:\n" + getEstado());
